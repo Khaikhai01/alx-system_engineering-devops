01021b8db40b781cf7052596b0105546b34afad8
purity <shishchei@gmail.com>
2023-04-24 12:37:09 -0700

    json file

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,29 +1,21 @@
 #!/usr/bin/python3
 '''script that retrieves infromation about an employee using ID'''
 
-import csv
 import json
 import requests
-from sys import argv
 
 if __name__ == "__main__":
-    emp_id = argv[1]
     usr_json = requests.get('https://jsonplaceholder.typicode.com/users/' +
                             emp_id).json()
-    emp_name = usr_json.get('username')
-    todos = requests.get('https://jsonplaceholder.typicode.com/users/' +
-                         emp_id + '/todos').json()
-
-    json_file = emp_id + '.json'
+    todo_file = 'todo_all_employees.json'
     first_dict = {}
 
-    first_dict[emp_id] = []
 
-    for item in todos:
+    for item in usr_json:
         sec_dict = {}
         sec_dict["task"] = item.get("title")
         sec_dict["completed"] = item.get("completed")
-        sec_dict["username"] = emp_name
+        sec_dict["usernae"] = emp_name
         first_dict.get(emp_id).append(sec_dict)
 
     with open(json_file, 'w') as jsfile:
