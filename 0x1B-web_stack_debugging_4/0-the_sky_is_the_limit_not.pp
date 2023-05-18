a7977ef8229f80a38700848b8622c2343f2b0f98
Purity <shishchei@gmail.com>
2023-05-18 12:03:25 -0700

    Debugging

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# This manuscript increases the amount of traffic an Nginx server can handle
+
+exec { 'ngix-conf':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
